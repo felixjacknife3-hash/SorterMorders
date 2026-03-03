6e15d4ecde22dcf7e490b3ad50d19e3ba63a3cf2
felixjacknife3-hash <felixjacknife3@gmail.com>
2026-03-03 16:43:42 -0700

    Fixed double jump

--- PlayerController.gd
+++ PlayerController.gd
@@ -7,7 +7,7 @@ class_name Player
 @export var baseSpeed: float = 3
 @export_subgroup("Jumps")
 @export var jumpVel: float = 2
-@export var dJumpVel: float = 1.5
+@export var maxJumps: int = 2
 @export_subgroup("Sensitivity")
 @export var sens: float = 2.2
 @export_group("Nodes")
@@ -15,24 +15,48 @@ class_name Player
 @export var head: Node3D
 
 var speed: float = 3
+var jumps = 0
+
+var captured: bool = true
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
+	#Head Movement Code
+	if event is InputEventMouseMotion and captured:
 		head.rotate_x(deg_to_rad(-event.relative.y * sens))
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		head.rotation_degrees.x = clamp(head.rotation_degrees.x, -80, 80)
 
 func _physics_process(delta: float) -> void:
+	#Leave Captured Mouse Mode
+	if Input.is_action_just_pressed("Esc"):
+		if captured:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			captured = not captured
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			captured = true
+		
+	#Jump Region
+	#region
+	#Set Double jump to 0 on touching ground
+	if is_on_floor():
+		jumps = 0
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and jumps < maxJumps:
 		velocity.y = jumpVel
-
+		jumps += 1
+	#endregion
+	#Movement Region
+	#region
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -41,5 +65,6 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
+	#endregion
 
 	move_and_slide()
