d95697a1077d08a737a417f0df99b6cf309ee432
felixjacknife3-hash <felixjacknife3@gmail.com>
2026-03-04 17:52:39 -0700

    Cleaned player code a bit

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,33 +1,43 @@
 extends CharacterBody3D
 class_name Player
 
+# the monstrosity
 @export_group("Numbers")
+
 @export_subgroup("Speeds")
 @export var sprintSpeed: float = 5
 @export var baseSpeed: float = 3
+
 @export_subgroup("Jumps")
 @export var jumpVel: float = 2
 @export var maxJumps: int = 2
+
 @export_subgroup("Camera")
 @export var sens: float = 1.6
 @export var minXRot: float = -70
 @export var maxXRot: float = 80
+
 @export_subgroup("Drag")
 @export var strength := 200.0
 @export var damping := 3.0
 @export_group("Bools")
+
 @export_group("Nodes")
+
 @export_subgroup("Misc 3D")
 @export var head: Node3D
 @export var environment: TimedEnvironment
 @export var light: SpotLight3D
+
 @export_subgroup("Drag")
 @export var ray: dragRay
 @export var dragTo: Marker3D
+
 @export_group("Resources")
 @export var shapeHeld: Shape3D
 @export var shapeUnHeld: Shape3D
 
+#non-editor vars
 var speed: float = 3
 var jumps = 0
 
@@ -37,11 +47,16 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
+	#cam movement logic
+	#region
 	if event is InputEventMouseMotion and captured:
 		head.rotate_x(deg_to_rad(-event.relative.y * sens))
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		head.rotation_degrees.x = clamp(head.rotation_degrees.x, minXRot, maxXRot)
+	#endregion
 
+#drag Logic
+#region
 func drag(collider: DraggableObject) -> void:
 	ray.shape = shapeHeld
 	var target := dragTo.global_position
@@ -49,6 +64,7 @@ func drag(collider: DraggableObject) -> void:
 	
 	collider.apply_central_force(diff * strength)
 	collider.linear_velocity *= (1.0 - damping * get_physics_process_delta_time())
+#endregion
 
 func _physics_process(delta: float) -> void:
 	light.visible = not environment.day
